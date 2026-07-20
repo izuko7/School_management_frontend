166df9118992a833f637b0c148cd46ee9619d47b
Campbell Miha <izuko7mha@gmail.com>
2026-07-20 17:04:46 +0000

    correction du fichier database.js

--- db/database.js
+++ db/database.js
@@ -1,11 +1,10 @@
 import Database from "better-sqlite3";
+import path from "path";
 
 // création de la base de donnée
-
-const db = new Database('database.db');
+const db = new Database(path.join(import.meta.dirname, 'database.db'));
 
 // activation des clés étrangères
-
 db.pragma('foreign_keys = ON');
 
 export default db;
