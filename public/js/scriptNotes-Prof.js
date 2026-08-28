a5bff6fe62494a9f5cb76392572c8cef354f8ae4
izuko7 <izuko7mha@gmail.com>
2026-08-28 09:02:24 +0000

    mise à jour de la page note-prof et création du fichier sciptNote-Prof.js

---
+++ public/js/scriptNotes-Prof.js
@@ -0,0 +1,85 @@
+// vérification du token de l'enseignant 
+const token = localStorage.getItem('token');
+const role = localStorage.getItem('role');
+
+if (!token || role !== 'prof') {
+    window.location.href = '/login';
+}
+
+const fetchAuth = async (url, method = 'GET', body = null) => {
+
+    const options = {
+        method: method,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    };
+
+    if (body) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(body);
+    }
+
+    const reponse = await fetch(url, options);
+
+    if (reponse.status === 401) {
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
+        window.location.href = '/login';
+        throw new Error("Token expiré");
+    }
+
+    return reponse.json();
+};
+
+const logOut = document.getElementById('boutonDeconnexion');
+logOut.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+    window.location.href = '/accueil'
+})
+
+const boutonBurger = document.getElementById('boutonBurger');
+const boutonFermerSidebar = document.getElementById('boutonFermerSidebar');
+const sidebar = document.querySelector('.sidebar');
+
+boutonBurger.addEventListener('click', function () {
+    sidebar.classList.add('ouvert');
+});
+
+boutonFermerSidebar.addEventListener('click', function () {
+    sidebar.classList.remove('ouvert');
+});
+
+function decoderToken(token) {
+    const playLoad = token.split('.')[1];
+    const decoded = JSON.parse(atob(playLoad));
+    return decoded;
+}
+
+const utilisateur = decoderToken(token);
+
+const donneUsers = async () => {
+    // Encore plus clean !
+    const users = await fetchAuth(`/users/${utilisateur.id}`);
+
+    let initial;
+    const mot = users.name.split(' ');
+
+    if (mot.length >= 2) {
+        initial = `${mot[0][0]}${mot[1][0]}`
+    } else {
+        initial = mot[0].substring(0, 2).toUpperCase();
+    }
+
+    const profilNom = document.getElementById('profilNom');
+    const profilRole = document.getElementById('profilRole');
+    const profilInitial = document.getElementById('profilAvatar');
+
+    profilNom.textContent = users.name;
+    profilRole.textContent = users.role;
+    profilInitial.textContent = initial;
+}
+
+donneUsers();
