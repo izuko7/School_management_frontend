14e0409b815416d978f26b83f5905ed720696a3b
izuko7 <izuko7mha@gmail.com>
2026-08-30 07:31:33 +0000

    création de la page présence prof

--- services/studentService.js
+++ services/studentService.js
@@ -5,7 +5,6 @@ import { dayjs } from "../config/date.js";
 // Créer un étudiant
 const createStudent = (matricule, nom, prenom, date_naissance, classe_id, user_id) => {
 
-    console.log(date_naissance)
     
     if(!dayjs(date_naissance, "DD/MM/YYYY", true).isValid()){
         throw new Error(`Date de naissance invalide : ${date_naissance}`);
