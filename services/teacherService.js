206d113ab2c22fcab3ad22ff2ddaa8cc04b53e27
izuko7 <izuko7mha@gmail.com>
2026-08-31 06:56:45 +0000

    mise à jour des services et controlers teachers

--- services/teacherService.js
+++ services/teacherService.js
@@ -2,60 +2,59 @@ import db from "../db/database.js";
 import Teacher from "../models/teacherModel.js";
 
 // Créer un enseignant
-const createTeacher = (matricule, nom, prenom, user_id) => {
-    
+const createTeacher = async (matricule, nom, prenom, user_id) => {
+
     // Vérifier que l'utilisateur avec le rôle prof
-    const user = db.prepare(`SELECT * FROM users WHERE id = ?`).get(user_id)
-    if(!user){
+    const user = await (await db.prepare(`SELECT * FROM users WHERE id = ?`)).get(user_id);
+    if (!user) {
         throw new Error(`Aucun utilisateur trouvé id : ${user_id}`);
     }
 
-    if(user.role !== 'prof'){
+    if (user.role !== 'prof') {
         throw new Error(`L'utilisateur avec l'id ${user_id} n'a pas le rôle "proffeseur"`);
     }
 
     // Vérifier que l'uilisateur n'est pas déjà lié à un professeur
-    const dejaLie = db.prepare(`SELECT * FROM teachers WHERE user_id = ?`).get(user_id);
-    if(dejaLie){
+    const dejaLie = await (await db.prepare(`SELECT * FROM teachers WHERE user_id = ?`)).get(user_id);
+    if (dejaLie) {
         throw new Error(`Ce compte est déjà lie à l'enseignant ${dejaLie.nom}`);
     }
 
     const teacher = new Teacher(matricule, nom, prenom, user_id);
-    const insertTeacher = db.prepare(`
+    const insertTeacher = await db.prepare(`
             INSERT OR IGNORE INTO teachers (matricule, nom, prenom, user_id)
             VALUES(?, ?, ?, ?)
         `);
     const result = insertTeacher.run(teacher.matricule, teacher.nom, teacher.prenom, teacher.user_id);
 
-    if(result.changes === 0){
+    if (result.changes === 0) {
         throw new Error(`L'enseignant ${nom} n'a pas pu être créer`);
     }
 
     return result;
 };
 
-
 // Afficher tout les enseignants
-const getAllTeachers = ()=> {
-    return db.prepare(`SELECT * FROM  teachers`).all();
+const getAllTeachers = async () => {
+    const result = await db.prepare(`SELECT * FROM teachers`);
+    return result.all();
 };
 
 // Afficher un enseignant grâce à son matricule
-
-const getTeacherByMatricule = (matricule)=> {
-    const teacher = db.prepare(`SELECT * FROM teachers WHERE matricule = ?`).get(matricule);
+const getTeacherByMatricule = async (matricule) => {
+    const teacher = (await db.prepare(`SELECT * FROM teachers WHERE matricule = ?`)).get(matricule);
     return teacher;
 };
 
-// afficher un enseignant grâce à son id 
-const getTeacherById = (id)=> {
-    const teacher = db.prepare(`SELECT * FROM teachers WHERE id = ?`).get(id);
+// Afficher un enseignant grâce à son id
+const getTeacherById = async (id) => {
+    const teacher = (await db.prepare(`SELECT * FROM teachers WHERE id = ?`)).get(id);
     return teacher;
-}
+};
 
 // Modifier un enseignant
-const updateTeacher = (id, data) =>{
-    const currentTeacher = getTeacherById(id);
+const updateTeacher = async (id, data) => {
+    const currentTeacher = await getTeacherById(id);
 
     // Sécurité : vérifier si l'enseignant existe
     if (!currentTeacher) {
@@ -65,24 +64,23 @@ const updateTeacher = (id, data) =>{
     const nom = data.nom ?? currentTeacher.nom;
     const prenom = data.prenom ?? currentTeacher.prenom;
 
-    const result = db.prepare(`
+    const result = (await db.prepare(`
             UPDATE teachers SET nom = ?, prenom = ?
             WHERE id = ?
-        `).run(nom, prenom, id);
+        `)).run(nom, prenom, id);
     return result;
 };
 
 // Supprimer un enseignant
-const deleteTeacher = (id)=> {
-    const current = getTeacherById(id);
-    if(!current){
+const deleteTeacher = async (id) => {
+    const current = await getTeacherById(id);
+    if (!current) {
         throw new Error(`Enseignant avec l'id ${id} introuvable`);
     }
-    const result = db.prepare(`DELETE FROM teachers WHERE id = ?`).run(id);
-    const deleteUserTeacher = db.prepare(`DELETE FROM users WHERE id = ?`).run(current.user_id);
-    
+    const result = (await db.prepare(`DELETE FROM teachers WHERE id = ?`)).run(id);
+    await (await db.prepare(`DELETE FROM users WHERE id = ?`)).run(current.user_id);
+
     return result;
 };
 
-
-export{ createTeacher, getAllTeachers, getTeacherByMatricule, getTeacherById, updateTeacher, deleteTeacher}  
+export { createTeacher, getAllTeachers, getTeacherByMatricule, getTeacherById, updateTeacher, deleteTeacher };
